75260abe2276e0dae0ec6d544e448181ee3d8e9a
beanzhang <beanzhang@aliyun.com>
2020-07-11 02:06:33 +0800

    test

--- BeanZhang.WebAPI/Controllers/WeatherForecastController.cs
+++ BeanZhang.WebAPI/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace BeanZhang.WebAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //test
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
