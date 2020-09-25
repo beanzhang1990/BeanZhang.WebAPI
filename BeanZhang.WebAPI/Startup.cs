dacca8bf3ed1dd4a0d5484597e9402f7d009f55e
beanzhang <beanzhang@aliyun.com>
2020-09-25 09:30:37 +0800

    添加swagger

--- BeanZhang.WebAPI/Startup.cs
+++ BeanZhang.WebAPI/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BeanZhang.WebAPI
 {
@@ -24,6 +26,15 @@ namespace BeanZhang.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "BeanZhang API",
+                    Description = "Web API"
+                });
+            });
             services.AddControllers();
         }
 
@@ -34,7 +45,11 @@ namespace BeanZhang.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BeanZhang API V1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
